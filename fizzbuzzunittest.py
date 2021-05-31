7c58da08c63273632652df5ba1904cb772829ce1
rajamong <63121185+rajamong@users.noreply.github.com>
2021-05-31 13:26:53 -0700

    used unittest to check the accuracy of the fizz functionality

--- fizzbuzzunittest.py
+++ fizzbuzzunittest.py
@@ -1,10 +1,17 @@
+# imports the unittest functionality
 import unittest
-import fizzbuzz 
-
-class TestCase(unittest.TestCase):
-    def test1(self):
-        self.assertEqual(fizzbuzz, 'Fizz')
+# import fizzbuzz function from fizzbuzz class
+from fizzbuzz import fizzbuzz # type: ignore
 
 
-if __name__ == '__main__':
+class testCase(unittest.TestCase):
+    # tests the functionality of the fizz,
+    # ensures it prints fizz for values that
+    # are evenly divisible by 3
+    def testfizz(self):
+        for number in [3, 12, 21]:
+            assert fizzbuzz(number) == 'Fizz'
+
+
+if __name__ == "__main__":
     unittest.main()
