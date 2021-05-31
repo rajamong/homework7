5a7de13932417fb16ee4b1a3de7511c3ac8b1b99
rajamong <63121185+rajamong@users.noreply.github.com>
2021-05-31 13:58:20 -0700

    updated my leapyear.py file to output strings instead of true/false values !!

--- leapyear.py
+++ leapyear.py
@@ -1,16 +1,16 @@
 # creates a program to check whether a year is a leap year
-def checkyear(year):
+def leapyear(year):
                 # if the year is evenly divisible by 4
         if (year % 4) == 0:
                 # if the year is evenly divisible by 100
                 if (year % 100) == 0:
                         # if the year is evenly divisible by 400
                         if (year % 400) == 0:
-                                return True
+                                return "True"
                         else:
-                                return False
+                                return "False"
                 else:
-                        return True
+                        return "True"
         else:
-                return False
+                return "False"
 
