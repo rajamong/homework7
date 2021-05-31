1711d1cc6991844df90eba05f2ffef99113ce296
rajamong <63121185+rajamong@users.noreply.github.com>
2021-05-31 13:41:26 -0700

    updated my leapyear function within my leapyear.py program !!

--- leapyear.py
+++ leapyear.py
@@ -1,4 +1,16 @@
 # creates a program to check whether a year is a leap year
 def checkyear(year):
-        pass
+                # if the year is evenly divisible by 4
+        if (year % 4) == 0:
+                # if the year is evenly divisible by 100
+                if (year % 100) == 0:
+                        # if the year is evenly divisible by 400
+                        if (year % 400) == 0:
+                                return True
+                        else:
+                                return False
+                else:
+                        return True
+        else:
+                return False
 
