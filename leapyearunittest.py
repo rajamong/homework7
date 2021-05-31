0200bd672f0e8e11719ae249db53e66d42db8153
rajamong <63121185+rajamong@users.noreply.github.com>
2021-05-31 13:43:52 -0700

    created a unittest that tests functionality with a valid leap year input !!

--- leapyearunittest.py
+++ leapyearunittest.py
@@ -1,13 +1,19 @@
+# imports unittest functionality
 import unittest
-import leapyear 
+# imports leapyear class from leapyear program
+from leapyear import checkyear # type: ignore
 
-class TestCase(unittest.TestCase):
-    def test1(self):
-        self.assertEqual(leapyear, True)
+class testCase(unittest.TestCase):
+    def setUp(self):
+        self.leap = leapyear()
+
+    # tests the leap year checking functionality with a valid leap year
+    def validyear(self):
+        self.assertEqual(self.leap.checkyear(2020), (True))
 
 
-if __name__ == '__main__':
-    unittest.main() 
+if __name__ == "__main__":
+    unittest.main()
 
 
 
